bc4280aa7d95bfc6f306bd51a3d7d6d43b4d7169
Robert Gladson <rkgladson@gmail.com>
2018-11-22 10:58:07 -0500

    Corrected test case to fix NPM Install

--- test/myTypescriptApp.ts
+++ test/myTypescriptApp.ts
@@ -1,7 +1,5 @@
 import FChatLib from "../src/FChatLib";
-import Config from "./config/Config";
-
-let options = require(__dirname+'/config/config.js');
+import Config from "./config/Config.example";
 let myFchatBot = new FChatLib(new Config());
 myFchatBot.connect();
 console.log("ok");
